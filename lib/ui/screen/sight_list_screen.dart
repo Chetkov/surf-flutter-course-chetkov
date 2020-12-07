5c98ad1517738167a3c307b93a842c14e7b55435
v.chetkov <chekov.valeriy@gmail.com>
2020-12-07 22:06:06 +0300

    string constants moved to sight_list_screen_const.dart

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:places/ui/screen/sight_list_screen_const.dart';
 
 class SightListScreen extends StatefulWidget {
   @override
@@ -10,38 +9,22 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('FloatingActionButton was pressed!');
-        },
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: BOTTOM_NAVIGATION_BAR_ITEM_HOME
-        ),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: BOTTOM_NAVIGATION_BAR_ITEM_MESSAGES,
-        ),
-      ]),
       appBar: AppBar(
-        title: Text(APP_BAR_TITLE),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.help),
-              onPressed: () {
-                print('Help was pressed');
-              }),
-          IconButton(
-              icon: Icon(Icons.question_answer),
-              onPressed: () {
-                print('FAQ was pressed');
-              })
-        ],
-      ),
-      drawer: Drawer(),
+        title: Text(
+          'Список\nинтересных мест',
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Roboto',
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.bold,
+            fontSize: 32,
+          ),
+          textAlign: TextAlign.left,
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        toolbarHeight: 100,
+      )
     );
   }
 }
