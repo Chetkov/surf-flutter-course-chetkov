b316662c489e1736432d605b80e20b9534049b94
v.chetkov <chekov.valeriy@gmail.com>
2020-12-14 03:46:48 +0300

    lesson 4.3.3

---
+++ lib/ui/screen/sight_details_screen.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:places/domain/sight.dart';
+
+var defaultTextStyle = TextStyle(
+  decoration: TextDecoration.none,
+  fontFamily: 'Roboto',
+  color: Color(0xff3B3E5B),
+);
+
+class SightDetailsScreen extends StatefulWidget {
+  final Sight _sight;
+
+  SightDetailsScreen(this._sight);
+
+  @override
+  _SightDetailsState createState() => _SightDetailsState(_sight);
+}
+
+class _SightDetailsState extends State<SightDetailsScreen> {
+  final Sight _sight;
+
+  _SightDetailsState(this._sight);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SightDetailsGallery(_sight),
+            Container(
+              margin: EdgeInsets.only(top: 24, left: 16, right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    _sight.name,
+                    textAlign: TextAlign.left,
+                    style: defaultTextStyle.copyWith(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 2),
+                    child: Wrap(
+                      spacing: 16,
+                      children: [
+                        Text(
+                          _sight.type,
+                          style: defaultTextStyle.copyWith(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'Закрыто до 09:00',
+                          style: TextStyle(
+                            color: Color(0xff7C7E92),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 24),
+                    child: Text(
+                      _sight.details,
+                      style: defaultTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 24),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Color(0xff4CAF50),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      spacing: 10,
+                      children: [
+                        Icon(
+                          Icons.map,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'ПОСТРОИТЬ МАРШРУТ',
+                          style: defaultTextStyle.copyWith(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    height: 48,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 24),
+                    height: 0.6,
+                    color: Colors.black.withAlpha(56),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 8, bottom: 8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 48,
+                            alignment: Alignment.center,
+                            child: Wrap(
+                              alignment: WrapAlignment.center,
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              spacing: 14,
+                              children: [
+                                Icon(
+                                  Icons.calendar_today,
+                                  color: Colors.black.withAlpha(56),
+                                ),
+                                Text(
+                                  'Запланировать',
+                                  style: defaultTextStyle.copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black.withAlpha(56),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 48,
+                            alignment: Alignment.center,
+                            child: Wrap(
+                              alignment: WrapAlignment.center,
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              spacing: 14,
+                              children: [
+                                Icon(
+                                  Icons.favorite_border,
+                                  color: Color(0xff3B3E5B),
+                                ),
+                                Text(
+                                  'В избранное',
+                                  style: defaultTextStyle.copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xff3B3E5B),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SightDetailsGallery extends StatelessWidget {
+  final Sight _sight;
+
+  SightDetailsGallery(this._sight);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 360,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(_sight.imageUrl), fit: BoxFit.cover),
+          ),
+        ),
+        Positioned(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.arrow_back_ios_sharp),
+              onPressed: () {},
+            ),
+          ),
+          top: 36,
+          left: 16,
+        )
+      ],
+    );
+  }
+}
